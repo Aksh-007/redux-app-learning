af390eb936e9a131bb87d3aeecc99a5d0e9a7a1e
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-17 18:14:33 +0530

    created home components

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    
+    const value = 20
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </div>
+  )
+}
+
+export default Home
